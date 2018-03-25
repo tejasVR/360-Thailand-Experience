2d40dae44a9d300cf5c7b6aa091407563e116429
Tejas Shroff <tejasshroff24@gmail.com>
2018-03-25 19:28:39 -0400

    Pushing project out of sub folder and into separate repo

---
+++ Assets/Project/Scripts/SceneChanger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BangkokTemple()
+    {
+        SceneManager.LoadScene("3. Bangkok Temple", LoadSceneMode.Single);
+    }
+
+    public void PhuketTemple()
+    {
+        SceneManager.LoadScene("4. Phuket Temple", LoadSceneMode.Single);
+    }
+
+    public void Opening()
+    {
+        SceneManager.LoadScene("2. Opening", LoadSceneMode.Single);
+    }
+
+    public void Ending()
+    {
+        SceneManager.LoadScene("5. Ending", LoadSceneMode.Single);
+    }
+}
